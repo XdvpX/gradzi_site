85cfa160fed6e4317b181a5bc8ca3108ddce36a1
Srinath DVP <srinathdamodaravenkatapavan@gmail.com>
2025-09-27 12:30:20 +0530

    fix in deployment

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,185 +1,146 @@
+// public/assets/js/main.js
 
-(function($) {
+window.initMainJS = function () {
+  (function ($) {
+    var $window = $(window),
+        $body = $('body'),
+        $sidebar = $('#sidebar');
 
-	var	$window = $(window),
-		$body = $('body'),
-		$sidebar = $('#sidebar');
+    // Breakpoints.
+    breakpoints({
+      xlarge:   [ '1281px',  '1680px' ],
+      large:    [ '981px',   '1280px' ],
+      medium:   [ '737px',   '980px'  ],
+      small:    [ '481px',   '736px'  ],
+      xsmall:   [ null,      '480px'  ]
+    });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+    // Hack: Enable IE flexbox workarounds.
+    if (browser.name == 'ie')
+      $body.addClass('is-ie');
 
-	// Hack: Enable IE flexbox workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
+    // Play initial animations on page load.
+    $window.on('load', function() {
+      window.setTimeout(function() {
+        $body.removeClass('is-preload');
+      }, 100);
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Forms.
+    $('form').on('click', '.submit', function(event) {
+      event.stopPropagation();
+      event.preventDefault();
+      $(this).parents('form').submit();
+    });
 
-	// Forms.
+    // Sidebar.
+    if ($sidebar.length > 0) {
+      var $sidebar_a = $sidebar.find('a');
 
-		// Hack: Activate non-input submits.
-			$('form').on('click', '.submit', function(event) {
+      $sidebar_a
+        .addClass('scrolly')
+        .on('click', function() {
+          var $this = $(this);
 
-				// Stop propagation, default.
-					event.stopPropagation();
-					event.preventDefault();
+          // External link? Bail.
+          if ($this.attr('href').charAt(0) != '#')
+            return;
 
-				// Submit form.
-					$(this).parents('form').submit();
+          // Deactivate all links.
+          $sidebar_a.removeClass('active');
 
-			});
+          // Activate link + lock it.
+          $this.addClass('active').addClass('active-locked');
+        })
+        .each(function() {
+          var $this = $(this),
+              id = $this.attr('href'),
+              $section = $(id);
 
-	// Sidebar.
-		if ($sidebar.length > 0) {
+          if ($section.length < 1) return;
 
-			var $sidebar_a = $sidebar.find('a');
+          // Scrollex.
+          $section.scrollex({
+            mode: 'middle',
+            top: '-20vh',
+            bottom: '-20vh',
+            initialize: function() {
+              $section.addClass('inactive');
+            },
+            enter: function() {
+              $section.removeClass('inactive');
 
-			$sidebar_a
-				.addClass('scrolly')
-				.on('click', function() {
+              if ($sidebar_a.filter('.active-locked').length == 0) {
+                $sidebar_a.removeClass('active');
+                $this.addClass('active');
+              }
+              else if ($this.hasClass('active-locked'))
+                $this.removeClass('active-locked');
+            }
+          });
+        });
+    }
 
-					var $this = $(this);
+    // Scrolly.
+    $('.scrolly').scrolly({
+      speed: 1000,
+      offset: function() {
+        if (breakpoints.active('<=large')
+          && !breakpoints.active('<=small')
+          && $sidebar.length > 0)
+          return $sidebar.height();
 
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
+        return 0;
+      }
+    });
 
-					// Deactivate all links.
-						$sidebar_a.removeClass('active');
+    // Spotlights.
+    $('.spotlights > section')
+      .scrollex({
+        mode: 'middle',
+        top: '-10vh',
+        bottom: '-10vh',
+        initialize: function() {
+          $(this).addClass('inactive');
+        },
+        enter: function() {
+          $(this).removeClass('inactive');
+        }
+      })
+      .each(function() {
+        var $this = $(this),
+            $image = $this.find('.image'),
+            $img = $image.find('img'),
+            x;
 
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
+        $image.css('background-image', 'url(' + $img.attr('src') + ')');
 
-				})
-				.each(function() {
+        if (x = $img.data('position'))
+          $image.css('background-position', x);
 
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
+        $img.hide();
+      });
 
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
+    // Features.
+    $('.features')
+      .scrollex({
+        mode: 'middle',
+        top: '-20vh',
+        bottom: '-20vh',
+        initialize: function() {
+          $(this).addClass('inactive');
+        },
+        enter: function() {
+          $(this).removeClass('inactive');
+        }
+      });
 
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '-20vh',
-							bottom: '-20vh',
-							initialize: function() {
+  })(jQuery);
+};
 
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($sidebar_a.filter('.active-locked').length == 0) {
-
-										$sidebar_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		}
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
-
-				// If <=large, >small, and sidebar is present, use its height as the offset.
-					if (breakpoints.active('<=large')
-					&&	!breakpoints.active('<=small')
-					&&	$sidebar.length > 0)
-						return $sidebar.height();
-
-				return 0;
-
-			}
-		});
-
-	// Spotlights.
-		$('.spotlights > section')
-			.scrollex({
-				mode: 'middle',
-				top: '-10vh',
-				bottom: '-10vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
-			})
-			.each(function() {
-
-				var	$this = $(this),
-					$image = $this.find('.image'),
-					$img = $image.find('img'),
-					x;
-
-				// Assign image.
-					$image.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Set background position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
-
-				// Hide <img>.
-					$img.hide();
-
-			});
-
-	// Features.
-		$('.features')
-			.scrollex({
-				mode: 'middle',
-				top: '-20vh',
-				bottom: '-20vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
-			});
-
-})(jQuery);
+// Run once when DOM is ready
+document.addEventListener("DOMContentLoaded", function () {
+  if (typeof window.initMainJS === "function") {
+    window.initMainJS();
+  }
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-export default function App({ children }){
-  return (
-    <div>
-      {children}
-    </div>
-  )
+import { useEffect } from "react";
+
+export default function App({ children }) {
+  useEffect(() => {
+    setTimeout(() => {
+      if (typeof window.initMainJS === "function") {
+        window.initMainJS();
+      }
+    }, 300);
+  }, []);
+
+  return <div>{children}</div>;
 }
